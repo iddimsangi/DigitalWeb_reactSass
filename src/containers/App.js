097b52a665b30e8baff56c6b702d7f04eee30548
iddimsangi <adammsangi@gmail.com>
2021-11-24 17:29:16 +0300

    why choose us section added

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,7 @@ import wave from "../images/wave1.png";
 import servc1 from "../images/service1.png";
 import servc2 from "../images/service2.png";
 import servc3 from "../images/service3.png";
+import technology from "../images/network.png";
 import "./App.scss";
 
 function App() {
@@ -50,14 +51,17 @@ function App() {
         <img src={wave} alt="wave-img" className="App-header--wave" />
       </header>
       <div className="About-section">
-        <h1>what we do?</h1>
+        <h1 className="heading">what we do ?</h1>
         <div className="About-section--details">
           <div className="About-section--details--detail">
             <img src={servc1} alt="service-1" />
             <div>
               <h3>growing marketing</h3>
               <p>
-              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable. If you are going to use a passage of
               </p>
             </div>
           </div>
@@ -66,7 +70,10 @@ function App() {
             <div>
               <h3>online branding</h3>
               <p>
-              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable. If you are going to use a passage of
               </p>
             </div>
           </div>
@@ -75,12 +82,35 @@ function App() {
             <div>
               <h3>animated ads</h3>
               <p>
-              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable. If you are going to use a passage of
               </p>
             </div>
           </div>
         </div>
-        <a href="#" className="btn">all services</a>
+        <a href="#" className="btn">
+          all services
+        </a>
+      </div>
+      <div className="why-section">
+        <h1 className="heading">why choose us ?</h1>
+        <div className="why-section--content">
+          <div>
+            <h1>why we're different</h1>
+            <ul>
+              <li>Contrary to popular is not simply </li>
+              <li>Conneed to be sure there isn't anything </li>
+              <li>Cocomes from sections 1.10.32 and 1.10.33 o </li>
+              <li>Contrary to popular belief, Lorem Isimply </li>
+              <li>Contrary to t simply </li>
+              <li>versions from the 1914 translation by H. Rackham. </li>
+              <li>Contrary , Lorem Ipsum is not simply </li>
+            </ul>
+          </div>
+          <img src={technology} alt="technology image" />
+        </div>
       </div>
     </div>
   );
